998ee2f3771013e05e720d69d553780cb076a996
Noor <mohammadnoor_ali@yahoo.com>
2019-11-09 18:54:51 +0500

    user routing done

--- src/components/app/app-routing.module.ts
+++ src/components/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
   { path: '', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
+
 ];
 
 @NgModule({
