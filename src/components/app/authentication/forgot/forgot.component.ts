1e2b8bd418ebaa114879ec638436402141c25722
Noor <mohammadnoor_ali@yahoo.com>
2019-11-10 19:11:50 +0500

    registration , forgot , login page done

--- src/components/app/authentication/forgot/forgot.component.ts
+++ src/components/app/authentication/forgot/forgot.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.component.html',
-  styleUrls: ['./forgot.component.scss']
+  styleUrls: ['./forgot.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ForgotComponent implements OnInit {
 
