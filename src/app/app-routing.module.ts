aca0bf9530f14f806a38ca15c07098fd43d82973
Noor <mohammadnoor_ali@yahoo.com>
2019-11-11 01:34:36 +0500

    FOLDER LOCATION CHANGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: '', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
+  { path: 'user', loadChildren: () => import('./components/user/user.module').then(m => m.UserModule) },
+  { path: '', loadChildren: () => import('./components/authentication/authentication.module').then(m => m.AuthenticationModule) }
 
 ];
 
--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { routeAnimation } from '../Constant/routeAnimation';
+import { routeAnimation } from '../../constant/routeAnimation';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-authentication',
