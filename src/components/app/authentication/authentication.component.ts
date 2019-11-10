3b2eaf5a96d7d8a13691841b754f230b561e8490
Noor <mohammadnoor_ali@yahoo.com>
2019-11-10 19:23:40 +0500

    width animation

--- src/components/app/authentication/authentication.component.ts
+++ src/components/app/authentication/authentication.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { routeAnimation } from '../Constant/routeAnimation';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.scss']
+  styleUrls: ['./authentication.component.scss'],
+  animations: [
+    routeAnimation
+    // animation triggers go here
+  ]
 })
 export class AuthenticationComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  activatedRoute(outlet: RouterOutlet){
+    if(outlet.isActivated) return outlet.activatedRoute;
   }
 
 }
